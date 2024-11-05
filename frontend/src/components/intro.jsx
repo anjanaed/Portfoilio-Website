c5df6199e378b3f51f238631fd59170599ceed48
Anjana Edirisinghe <anjanaape@gmail.com>
2024-11-05 17:35:12 +0530

    0.4

--- frontend/src/components/intro.jsx
+++ frontend/src/components/intro.jsx
@@ -23,7 +23,7 @@ const Intro=()=>{
                         <motion.h1 variants={motions(0.2)} initial="pre" animate="post" className="pb-16 text-4xl font-thin tracking-tight lg:mt-16 lg:text-7xl">
                             Anjana Edirisinghe
                         </motion.h1>
-                        <motion.span variants={motions(0.6)} initial="pre" animate="post" className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent text-center">
+                        <motion.span variants={motions(0.6)} initial="pre" animate="post" className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text lg:text-3xl text-2xl tracking-tight text-transparent text-center">
                             Student Blockchain Developer
                         </motion.span>
                         <motion.p variants={motions(1)} initial="pre" animate="post" className="my-2 max-w-xl py-6 font-light tracking-tighter lg:text-justify">
